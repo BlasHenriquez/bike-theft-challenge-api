3787302e5e50c7b13769a9387ec746894ed71e33
BlasHenriquez <blasdelcristo_95@hotmail.com>
2022-09-12 20:48:52 +0200

    install and configurate swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,27 @@ async function bootstrap() {
   if (enableCors) {
     app.enableCors();
   }
+
+  const config = new DocumentBuilder()
+    .setTitle('Control Bikes')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'refresh-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { filter: true },
+  });
+
   await app.listen(port);
 }
 bootstrap();
