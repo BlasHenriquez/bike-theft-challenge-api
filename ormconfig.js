e6f5a03a0dfa1c0948d75cef54ceba71c6fffd36
BlasHenriquez <blasdelcristo_95@hotmail.com>
2022-09-18 20:58:29 +0200

    configurate test

--- ormconfig.js
+++ ormconfig.js
@@ -7,8 +7,14 @@ module.exports = {
   database: process.env.POSTGRES_NAME,
   entities: ['dist/**/entities/*.entity{.ts,.js}'],
   migrations: ['dist/db/migrations/*{.ts,.js}'],
-  seeds: ['dist/db/seeds/*.js'],
-  factories: ['dist/db/factories/*.js'],
+  seeds:
+    process.env.NODE_ENV === 'test'
+      ? ['db/seeds/test/*seeds.ts']
+      : ['dist/db/seeds/*seeds.js'],
+      factories:
+      process.env.NODE_ENV === 'test'
+        ? ['db/factories/*.ts']
+        : ['dist/db/factories/*.js'],
   cli: {
     migrationsDir: 'db/migrations',
   },
