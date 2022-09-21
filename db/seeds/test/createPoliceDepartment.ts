2b0a239a7c946063396a381039f468d9cfbe69a3
BlasHenriquez <blasdelcristo_95@hotmail.com>
2022-09-22 01:08:54 +0200

    modified and create seeds and utils

--- db/seeds/test/createPoliceDepartment.ts
+++ db/seeds/test/createPoliceDepartment.ts
@@ -1,7 +1,7 @@
 import { Factory, Seeder } from 'typeorm-seeding';
 import { PoliceDepartment } from '../../../src/police-departments/entities/police-department.entity';
 
-export default class CreatePoliceDepartment implements Seeder {
+export default class CreatePoliceDepartmentTest implements Seeder {
   public async run(factory: Factory): Promise<any> {
     const policeDepartment = await factory(PoliceDepartment)().create();
 
