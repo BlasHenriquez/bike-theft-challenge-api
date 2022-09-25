d6d2c7e405572c526a109223451b805524998d74
BlasHenriquez <blasdelcristo_95@hotmail.com>
2022-09-25 15:14:01 +0200

    rename seed bike report and add bike owner

--- db/seeds/03-bikeReports.seeds.ts
+++ db/seeds/03-bikeReports.seeds.ts
@@ -3,13 +3,15 @@ import { Factory, Seeder } from 'typeorm-seeding';
 import { PoliceOfficer } from '../../src/police-officers/entities/police-officer.entity';
 import { Bike } from '../../src/bikes/entities/bike.entity';
 import { BikeReport } from '../../src/bike-reports/entities/bike-report.entity';
+import { BikeOwner } from '../../src/bike-owners/entities/bike-owner.entity';
 
 export default class CreateBikeReports implements Seeder {
   public async run(factory: Factory): Promise<any> {
     for (let i = 0; i < 20; i++) {
       const policeDepartment = await factory(PoliceDepartment)().create();
       const police = await factory(PoliceOfficer)().create();
-      const bike = await factory(Bike)().create();
+      const bikeOwner = await factory(BikeOwner)().create();
+      const bike = await factory(Bike)({ bikeOwner }).create();
 
       await factory(PoliceOfficer)({
         password: 'Prueba123>',
