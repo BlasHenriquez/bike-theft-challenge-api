4e960adb053789f52ad7069d39d980dbfda49997
BlasHenriquez <blasdelcristo_95@hotmail.com>
2022-09-25 15:19:28 +0200

    add unique faker name for departments

--- db/factories/police-department.factory.ts
+++ db/factories/police-department.factory.ts
@@ -5,7 +5,8 @@ import { PoliceDepartment } from '../../src/police-departments/entities/police-d
 define(PoliceDepartment, (_, context: Partial<PoliceDepartment>) => {
   const policeDepartment = new PoliceDepartment();
 
-  policeDepartment.name = context?.name || faker.name.firstName();
+  policeDepartment.name =
+    context?.name || faker.helpers.unique(faker.name.firstName);
 
   policeDepartment.policeOfficer = context?.policeOfficer;
 
