264f01b3798781750fab831dcaf56354f6258357
BlasHenriquez <blasdelcristo_95@hotmail.com>
2022-09-26 21:04:52 +0200

    fix seeds police in report to busy

--- db/seeds/03-bikeReports.seeds.ts
+++ db/seeds/03-bikeReports.seeds.ts
@@ -4,12 +4,15 @@ import { PoliceOfficer } from '../../src/police-officers/entities/police-officer
 import { Bike } from '../../src/bikes/entities/bike.entity';
 import { BikeReport } from '../../src/bike-reports/entities/bike-report.entity';
 import { BikeOwner } from '../../src/bike-owners/entities/bike-owner.entity';
+import { StatusPolice } from '../../src/utils/enum/police-officer.enum';
 
 export default class CreateBikeReports implements Seeder {
   public async run(factory: Factory): Promise<any> {
     for (let i = 0; i < 20; i++) {
       const policeDepartment = await factory(PoliceDepartment)().create();
-      const police = await factory(PoliceOfficer)().create();
+      const police = await factory(PoliceOfficer)({
+        status: StatusPolice.BUSY,
+      }).create();
       const bikeOwner = await factory(BikeOwner)().create();
       const bike = await factory(Bike)({ bikeOwner }).create();
 
